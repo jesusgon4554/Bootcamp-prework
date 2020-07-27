c4dc33320b6366fa326faf0a00dc077a65d5cca8
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-07-27 11:46:47 -0700

    Add files via upload

---
+++ Prework_JG/Module -  6/javascript.js
@@ -0,0 +1,19 @@
+<script src="javascript.js"></script>
+    
+    document.getElementById("growBtn").addEventListener("click", function(){
+        document.getElementById("box").style.height = "500px";
+    }); 
+
+    document.getElementById("blueBtn").addEventListener("click", function(){
+        document.getElementById("box").style.color = blue;
+    });
+
+    document.getElementById("fadeBtn").addEventListener("click", function(){
+        document.getElementById("box").style.opacity
+    });
+
+    document.getElementById("resetBtn").addEventListener("click", function(){
+        document.getElementById("box")
+  });
+
+</script>
